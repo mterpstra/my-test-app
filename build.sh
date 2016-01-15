7532963726e50772184087d10400fbff93f186ec
mterpstra <markterp@cloud.com>
2016-01-15 14:13:46 -0500

    Fix

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 go get github.com/go-sql-driver/mysql
 go install github.com/go-sql-driver/mysql
+go get github.com/tools/godep
+go install github.com/tools/godep
+godep
 go build -o bin/application
